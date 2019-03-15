72afa36034a26955d22c20825e1adaf1de83d834
Oscar Palafox <oscarpalafoxverna@gmail.com>
2019-03-15 18:25:45 +0000

    Indentation fixed

--- tailored_project/tailored/models.py
+++ tailored_project/tailored/models.py
@@ -4,44 +4,44 @@ from django.contrib.auth.models import User
 import datetime
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    picture = models.ImageField(upload_to = "profile_images", blank = True)
-    postcode = models.CharField(max_length = 8)
-    rating = models.IntegerField(default = 0)
-    phone = models.IntegerField(default = 0)
+	user = models.OneToOneField(User)
+	picture = models.ImageField(upload_to = "profile_images", blank = True)
+	postcode = models.CharField(max_length = 8)
+	rating = models.IntegerField(default = 0)
+	phone = models.IntegerField(default = 0)
 
-    def __str__(self):
-        return self.user.username
+	def __str__(self):
+		return self.user.username
 
 class Category(models.Model):
-    title = models.CharField(max_length = 128, unique = True)
-    section = models.ForeignKey(Section)
-    class Meta:
-        verbose_name_plural = "categories"
-    
-    def __str__(self):
-        return self.title
+	title = models.CharField(max_length = 128, unique = True)
+	section = models.ForeignKey(Section)
+	class Meta:
+		verbose_name_plural = "categories"
+	
+	def __str__(self):
+		return self.title
 
 class Section (models.Model):
-    name = models.CharField(max_length = 128, unique = True)
+	name = models.CharField(max_length = 128, unique = True)
 
-    def __str__(self):
-        return self.section
+	def __str__(self):
+		return self.section
 
 class Item(models.Model):
-    category = models.ForeignKey(Category)
-    section = models.ForeignKey(Section)
+	category = models.ForeignKey(Category)
+	section = models.ForeignKey(Section)
 
-    itemName = models.CharField(max_length = 128)
-    price = models.IntegerField(default = 0)
+	itemName = models.CharField(max_length = 128)
+	price = models.IntegerField(default = 0)
 
-    #itemPic = models.ImageField(upload_to = "item_images", blank = True)
-    description = models.TextField(blank = True)
-    datePosted = models.DateField(_("Date"),auto_now_add = True)
-    
-    sold = models.BooleanField(default = False)
-    dailyVisits = models.IntegerField(default = 0)
-    size = models.CharField(max_length = 128)
+	#itemPic = models.ImageField(upload_to = "item_images", blank = True)
+	description = models.TextField(blank = True)
+	datePosted = models.DateField(_("Date"),auto_now_add = True)
+	
+	sold = models.BooleanField(default = False)
+	dailyVisits = models.IntegerField(default = 0)
+	size = models.CharField(max_length = 128)
 
-    def __str__(self):
-        return self.itemName
+	def __str__(self):
+		return self.itemName
