75580de441a9a87a5638177cf87f629cc3260528
Lucas Prieto <2324138p@student.gla.ac.uk>
2019-03-16 11:58:58 +0000

    search

--- tailored_project/tailored/views.py
+++ tailored_project/tailored/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.db.model import Q
 # Create your views here.
+
+def search_bar(request):
+	search_bar=Search_bar()
+	if(request.method=='POST'):
+		search_bar=Search_bar(request.POST)
+		query=request.POST.get('search')
+		items= []
+		for word in query:
+				items+=Item.objects.filter(Q(itemName__icontains=word))
+		return render(items, 'search.html')
+
--- tailored_project/tailored_project/urls.py
+++ tailored_project/tailored_project/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
+	url(r'search/$', views.about, name='search'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
