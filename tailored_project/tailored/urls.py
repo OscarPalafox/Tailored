870a07296300c51ffd242fe5a0e8e118317229f0
Lucas Prieto <2324138p@student.gla.ac.uk>
2019-03-16 17:39:33 +0000

    search is working

--- tailored_project/tailored/urls.py
+++ tailored_project/tailored/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
-	url(r'index/$', views.home_page, name='home_page'),
+	url(r'^index/$', views.home_page, name='home_page'),
 	url(r'^tailored/search/$', views.search_bar, name='search'),
-	
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tailored_project/tailored/views.py
+++ tailored_project/tailored/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from django.db.model import Q
+from tailored.models import Item
+from tailored.forms import Search_bar
+from django.db.models import Q
 # Create your views here.
 
 def search_bar(request):
-	search_bar=Search_bar()
+	
 	if(request.method=='POST'):
-		search_bar=Search_bar(request.POST)
-		query=request.POST.get('search')
+
+		query=request.POST.get('search').split(" ")
 		items= []
 		for word in query:
-				items+=Item.objects.filter(Q(itemName__icontains=word))
-		return render(items, 'search.html')
+				items+=Item.objects.filter(Q(description__contains=word )|Q (title__contains=word))
+				print(items)
+		context_dict={}
+		context_dict['items']=items
+		return render(request, 'tailored/search.html',context_dict)
 
+def home_page(request):
+	
+	
+	
+	return render(request, 'tailored/index.html')
