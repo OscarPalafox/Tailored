09bbb6a6d81c389e016c8c81a5eaea34ecc2b80d
NicholasSperry <sperrynicholas@gmail.com>
2019-03-18 16:39:57 +0000

    Working add item

--- tailored_project/tailored/admin.py
+++ tailored_project/tailored/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from tailored.models import Section, Category, Item, UserProfile, Review, Size
 
-#class PersonAdmin(admin.ModelAdmin):
-	#readonly_fields = ('itemID', )
+class PersonAdmin(admin.ModelAdmin):
+	readonly_fields = ('itemID', )
 
 admin.site.register(Section)
 admin.site.register(Category)
-admin.site.register(Item)#, PersonAdmin)
+admin.site.register(Item, PersonAdmin)
 
 admin.site.register(UserProfile)
 admin.site.register(Review)
