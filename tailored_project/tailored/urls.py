dd5af80832bed8fd6653d7096d5a9545d7a16f0c
Oscar Palafox <oscarpalafoxverna@gmail.com>
2019-03-18 16:56:25 +0000

    Small tweaks

--- tailored_project/tailored/urls.py
+++ tailored_project/tailored/urls.py
@@ -8,11 +8,9 @@ urlpatterns = [
 	url(r'^add_category/$', views.add_category, name = 'add_category'),
 	url(r'^section/(?P<title>[-\w]+)/$', views.show_section, name = 'show_section'),
 	url(r'^category/(?P<title>[-\w]+)/$', views.show_category, name='show_category'),
-	url(r'^admin/', admin.site.urls),
 	url(r'^index/$', views.home_page, name='home_page'),
 	url(r'^tailored/search/$', views.search_bar, name='search'),
 	url(r'add_item/$', views.add_item, name="add_item"),
 	url(r'^home/$', views.home_page, name = 'home_page'),
 	url(r'^search/$', views.search_bar, name = 'search')
 ]
-
