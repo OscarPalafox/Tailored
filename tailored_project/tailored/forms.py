3799cf45517f7ac960de30ecc9f174495d3aef64
Youssef Ramadan <2299608r@student.gla.ac.uk>
2019-03-19 19:29:30 +0000

    fixed css for login and sign up templates

--- tailored_project/tailored/forms.py
+++ tailored_project/tailored/forms.py
@@ -63,7 +63,7 @@ class UserProfileForm(RegistrationFormTermsOfService, RegistrationFormUniqueEmai
 	picture = forms.ImageField(required = False)
 	postcode = forms.CharField(max_length = 8, validators = [RegexValidator(r'^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$')])
 	phone = forms.CharField(max_length = 8, validators = [RegexValidator(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.0-9]*$')], required = False)
-
+        field_order = ['first_name', 'last_name','postcode','picture','phone']
 
 """class ReviewForm(forms.ModelForm):
 	"""
