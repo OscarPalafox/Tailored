7ed2b88e06c94d1772f8e2618bf5f6c710d2cdb4
Youssef Ramadan <2299608r@student.gla.ac.uk>
2019-03-20 21:59:33 +0000

    testing help messages

--- tailored_project/static/js/main.js
+++ tailored_project/static/js/main.js
@@ -26,6 +26,10 @@
 			if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
 				return false;
 			}
+		} else if ($(input).attr('type') == 'password'){
+			if($('input').val() != $('#id_password2').val()) {
+				return false;
+			}
 		} else {
 			if ($(input).val().trim() == '') {
 				return false;
