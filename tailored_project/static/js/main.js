4c55ad04b7238c9ffc5cb0a20d3b78e45bd83805
Youssef Ramadan <2299608r@student.gla.ac.uk>
2019-03-21 23:35:46 +0000

    Sign up now shows errors

--- tailored_project/static/js/main.js
+++ tailored_project/static/js/main.js
@@ -5,53 +5,7 @@
 
 
 (function($) {
-	"use strict";
-	var input = $('.validate-input .input100');
-	$('.validate-form').on('submit', function() {
-		var check = true;
-		for (var i = 0; i < input.length; i++) {
-			if (validate(input[i]) == false) {
-				showValidate(input[i]);
-				check = false;
-			}
-		}
-		return check;
-	});
-	$('.validate-form .input100').each(function() {
-		$(this).focus(function() {
-			hideValidate(this);
-		});
-	});
-
-	function validate(input) {
-		if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-			if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-				return false;
-			}
-		} else if ($(input).attr('type') == 'password'){
-			if($('input').val() != $('#id_password2').val()) {
-				return false;
-			}
-		} else {
-			if ($(input).val().trim() == '') {
-				return false;
-			}
-		}
-	}
-
-	function showValidate(input) {
-		var thisAlert = $(input).parent();
-		$(thisAlert).addClass('alert-validate');
-	}
-
-	function hideValidate(input) {
-		var thisAlert = $(input).parent();
-		$(thisAlert).removeClass('alert-validate');
-	}
 	
-
-
-	});
 
 	$("#page2").click(function(){
 		var thisURL=(window.location.href).replace("http://127.0.0.1:8000","").replace("3/","");
@@ -247,7 +201,7 @@
           
             
 	}
-
+	
     $('.slider-range-price').each(function() {
         var min = jQuery(this).data('min');
         var max = jQuery(this).data('max');
@@ -272,6 +226,6 @@
         });
     });
 			
-
+	
 
 })(jQuery);
