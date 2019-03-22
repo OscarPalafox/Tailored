4da0b00507f565bbedf47c00ef0e9ae1b4a34476
Oscar Palafox <oscarpalafoxverna@gmail.com>
2019-03-22 17:12:23 +0000

    Small tweaks

--- tailored_project/tailored/forms.py
+++ tailored_project/tailored/forms.py
@@ -14,19 +14,18 @@ class Search_bar(forms.ModelForm):
 		model = Item
 		fields = ('search',)
 
+
 class ItemForm(forms.ModelForm):
-	title = forms.CharField(max_length = 128,
-		help_text = 'Please enter the name of the item: ')
+	title = forms.CharField(max_length = 128, help_text = 'Please enter the name of the item: ')
 	price = forms.DecimalField(help_text = 'Enter the price: ', min_value = 0, decimal_places = 2)
 
 	section = forms.ModelChoiceField(queryset = Section.objects.all(), help_text = 'Select a section: ')
 	category = forms.ModelChoiceField(queryset = Category.objects.all(), help_text = 'Select a category: ')
 
-	picture= forms.ImageField(required = False, 
-		help_text = 'Upload a picture of the item: ')
+	picture= forms.ImageField(required = False, help_text = 'Upload a picture of the item: ')
 	
-	description = forms.CharField(widget = forms.Textarea, 
-		help_text = 'Please give a brief description of the item.')
+	description = forms.CharField(widget = forms.Textarea,
+									help_text = 'Please give a brief description of the item.')
 	size = forms.ModelChoiceField(queryset = Size.objects.all(), help_text = 'Select the size: ')
 
 	# Inline class to provide additional information on the form
@@ -43,14 +42,15 @@ class UserProfileForm(RegistrationFormTermsOfService, RegistrationFormUniqueEmai
 	postcode = forms.CharField(max_length = 8, validators = [RegexValidator(r'^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$')])
 	phone = forms.CharField(max_length = 8, required = False, validators = [RegexValidator(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.0-9]*$')])
 
-	field_order = ["first_name","last_name", "phone","postcode", "picture"]
+	# Inline class to provide additional information on the form
+	class Meta:
+		field_order = ["first_name", "last_name", "phone", "postcode", "picture"]
 
 
 class ReviewForm(forms.ModelForm):
 	rating = forms.IntegerField(help_text = 'Rate the seller out of 5:', min_value = 0, max_value = 5)
 	review_text = forms.CharField(widget = forms.Textarea, 
 		help_text = 'Please give a brief review of the seller.', required = False)
-
 
 	def __init__(self, user_items, *args, **kwargs):
 		super(ReviewForm, self).__init__(*args, **kwargs)
@@ -74,7 +74,9 @@ class EditUserProfileForm(forms.ModelForm):
 	phone = forms.CharField(help_text = 'Phone: ', required = False, max_length = 8,
 						validators = [RegexValidator(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.0-9]*$')])
 
+	# Inline class to provide additional information on the form
 	class Meta:
+		#Provide an association between the ModelForm and a model
 		model = UserProfile
 		fields = ('first_name', 'last_name', 'picture', 'postcode', 'phone')
 
@@ -87,4 +89,3 @@ class EditItemForm(forms.ModelForm):
 	class Meta:
 		model = Item
 		fields = []
-
