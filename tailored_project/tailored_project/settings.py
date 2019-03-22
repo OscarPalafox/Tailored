34db7f545f7d289b122fc41720293d7449eaf372
Oscar Palafox <oscarpalafoxverna@gmail.com>
2019-03-22 15:49:58 +0000

    Debug undone

--- tailored_project/tailored_project/settings.py
+++ tailored_project/tailored_project/settings.py
@@ -26,7 +26,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'b12u&9(vx(teeeuultxj5azuugfp1t-j(ou01&^4a$7ftt!dwz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'tailored.pythonanywhere.com']
 
