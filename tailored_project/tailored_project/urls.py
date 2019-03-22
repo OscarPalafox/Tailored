3fd1d388c33f08c2ecdaf89f571314500fd63819
Oscar Palafox <oscarpalafoxverna@gmail.com>
2019-03-22 15:18:16 +0000

    Update urls.py

--- tailored_project/tailored_project/urls.py
+++ tailored_project/tailored_project/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
 	url(r'^', include('registration.backends.default.urls')),
 	url(r'^admin/', admin.site.urls),
 	url(r'^tailored/$', views.search_bar, name = 'home_page'),
-	url(r'^tailored/shop_bootstrap/$', views.search_bar, name='search'),
 	url(r'^tailored/(?P<search>[\w\-]+)/(?P<page>[\w\-]+)/$', views.search_bar, name='search'),
 	url(r'^tailored/(?P<search>[\w\-]+)/$', views.search_bar, name='search'),
 
