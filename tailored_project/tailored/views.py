30707fde2fdc1af53cdc9381b80ace75301c369f
Oscar Palafox <oscarpalafoxverna@gmail.com>
2019-05-23 20:06:25 +0200

    Small tweaks

--- tailored_project/tailored/views.py
+++ tailored_project/tailored/views.py
@@ -28,7 +28,7 @@ def delete(request, itemID):
 	except ValidationError:
 		raise Http404
 
-	if (item.seller.user != request.user):
+	if item.seller.user != request.user:
 		raise Http404
 	
 	item.delete()
@@ -48,17 +48,15 @@ def show_item(request, itemID):
 	context_dict = {}
 	search_form = Search_bar()
 	context_dict['search_bar'] = search_form
-	isSeller = request.user == item.seller.user
-	context_dict['isSeller'] = isSeller
 	context_dict['item'] = item
 	context_dict['seller_rating'] = range(int(round(item.seller.rating, 1)))
 
 	related = Item.objects.filter(category = item.category).exclude(itemID = item.itemID)
 	
-	if len(related)>3:
+	if len(related) > 3:
 		context_dict['trendingItems'] = related[0:3]
 	else:
-		context_dict['trendingItems']=related
+		context_dict['trendingItems'] = related
 
 	response = render(request, 'tailored/product.html', context_dict)
 	
@@ -68,7 +66,7 @@ def show_item(request, itemID):
 		
 	context_dict['itemID'] = item.itemID
 
-	if (item.seller.user != request.user):
+	if item.seller.user != request.user:
 		return response
 
 	sold_form = SoldItemForm()
@@ -110,7 +108,7 @@ def trending(request):
 
 	for item in Item.objects.order_by('-dailyVisits'):
 		#Include items that have been uploaded within the past day and havent been sold
-		if ((date.today() - item.datePosted).days <= 0) and (item.sold_to == None):
+		if (date.today() - item.datePosted).days <= 0 and item.sold_to == None:
 			if (len(trending) <= 5):
 				trending.append(item)
 		else:
@@ -118,7 +116,7 @@ def trending(request):
 			item.save()
 
 	#If there are not enough items in the trending list, add older items to the list
-	if (len(trending) <= 5):
+	if len(trending) <= 5:
 		for item in Item.objects.order_by('-dailyVisits'):
 			if ((len(trending) <= 5) and (item.sold_to == None) and (item not in trending)):
 				trending.append(item)
@@ -128,14 +126,18 @@ def trending(request):
 
 
 def show_seller_profile(request, seller_username):
+	"""This view shows the profile of the given seller."""
 	context_dict = {}
-	form = Search_bar()
-	context_dict['search_bar'] = form
+	search_form = Search_bar()
+	context_dict['search_bar'] = search_form
+
 	seller_user = get_object_or_404(User, username = seller_username)
 	context_dict['seller_user'] = seller_user
+	
 	seller_user_profile=get_object_or_404(UserProfile, user=seller_user)
 	selling = Item.objects.filter(seller = seller_user_profile)
 	context_dict['selling'] = selling[0:3]
+	
 	seller_user_profile = get_object_or_404(UserProfile, user = seller_user)
 	context_dict['seller_user_profile'] = seller_user_profile
 	context_dict['seller_rating'] = range(int(round(seller_user_profile.rating, 1)))
@@ -144,13 +146,11 @@ def show_seller_profile(request, seller_username):
 	itemList = []
 
 	for item in seller_items:
-		if (item.sold_to == None):
+		if item.sold_to == None:
 			itemList.append(item)
 
 	context_dict['seller_items'] = itemList
-
 	reviews_seller = Review.objects.filter(Q(item__in = Item.objects.filter(seller = seller_user_profile)))
-	
 	context_dict['reviews_seller'] = reviews_seller.order_by('-datePosted')
 
 	if request.user.is_authenticated():
@@ -162,7 +162,6 @@ def show_seller_profile(request, seller_username):
 													user__in = User.objects.filter(username = request.user))) &
 									Q(seller = seller_user_profile)
 									).exclude(itemID__in = items_reviewed)
-		
 		context_dict['items_to_review'] = items_to_review
 
 		if items_to_review:
@@ -181,10 +180,11 @@ def show_seller_profile(request, seller_username):
 					reviews_seller_updated = Review.objects.filter(Q(item__in = Item.objects.filter(
 																		seller = seller_user_profile)))
 					rating = 0
+					
 					for review_updated in list(reviews_seller_updated):
 						rating += review_updated.rating
+					
 					rating = round(rating/len(reviews_seller_updated), 1)
-
 					seller_user_profile.rating = rating
 					seller_user_profile.save()
 
@@ -197,13 +197,14 @@ def show_seller_profile(request, seller_username):
 
 @login_required
 def user_profile(request):
+	"""This view shows the profile of the authenticated user."""
 	user = User.objects.get(username = request.user)
 	user_profile = get_object_or_404(UserProfile, user = user)
 
 	context_dict = {}
-	form = Search_bar()
-	context_dict['search_bar']=form
-	context_dict["user_profile"] = user_profile
+	search_form = Search_bar()
+	context_dict['search_bar'] = search_form
+	context_dict['user_profile'] = user_profile
 	context_dict['user_rating'] = range(int(round(user_profile.rating, 0)))
 	
 	reviews_user = Review.objects.filter(Q(item__in = Item.objects.filter(seller = user_profile)))
@@ -256,12 +257,15 @@ def user_profile(request):
 					if user_form.cleaned_data[key] and user_profile.__dict__[key] != user_form.cleaned_data[key]:
 						user_profile.__dict__[key] = user_form.cleaned_data[key]
 				user_profile.save()
+
 				context_dict['item_form'] = item_form
 				return HttpResponseRedirect(reverse('tailored:index'))
 			else:
 				keys = list(user_form.cleaned_data.keys())
+				
 				for key in keys:
 					user_form.add_error(key, forms.ValidationError("You need to fill at least one field."))
+				
 				context_dict['item_form'] = item_form
 				context_dict['user_form'] = user_form
 				return render(request, 'tailored/user_profile.html', context_dict)
@@ -272,73 +276,78 @@ def user_profile(request):
 
 
 def search_bar(request, search = None):
-
+	"""This view takes care of searching objects."""
 	categories = Category.objects.all()
-	
-	
 
-	if(request.method == 'POST'):
-		check = request.POST.get('search')
-		if check != None:
-			if check != "":
-				check="-".join(check.split(" "))
-				return HttpResponseRedirect(reverse('tailored:search', kwargs = {'search': check}))
+	if request.method == 'POST':
+		search_form = Search_bar(request.POST)
+		if search_form.is_valid():
+			if search_form.cleaned_data['search']:
+				parameters = '-'.join(search_form.cleaned_data['search'].split(' '))
+				return HttpResponseRedirect(reverse('tailored:search', kwargs = {'search': parameters}))
 			else:
-				return HttpResponseRedirect(reverse('tailored:search', kwargs = {'search': 'all'}))
-		else:
-			return HttpResponseRedirect('all/')
+				return HttpResponseRedirect('all/')
 
 	context_dict = {}
 	form = Search_bar()
 	context_dict['search_bar'] = form
 	context_dict['categories'] = categories
-	items = []	
-	if search == "all" or search == None or search == "":
-		search = "all"
-		items = Item.objects.filter(sold_to=None)
+	items = []
+
+	if search == 'all' or search == None or search == '':
+		search = 'all'
+		items = Item.objects.filter(sold_to = None)
 	else:
 		if search != None:
-			search = search.split("-")
+			search = search.split('-')
 			for word in search:
 				items += Item.objects.filter((Q(description__contains = word ) | Q(title__contains = word)
-					| Q(category = word) | Q(section = word)) & Q(sold_to=None))
-			searchS = "_".join(search)
+					| Q(category = word) | Q(section = word)) & Q(sold_to = None))
+			searchS = '_'.join(search)
 			context_dict['search'] = searchS
 		else:
 			return home_page(request)
+	
 	maxi = 0
+	
 	for item in items:
 		if item.price > maxi:
 			maxi = item.price
-	context_dict['maxi'] = maxi+5
-
+	
+	context_dict['maxi'] = maxi + 5
 	context_dict['items'] = items
-	context_dict['pages']= int(len(items)/6)
+	context_dict['pages'] = int(len(items)/6)
 	
 	return render(request, 'tailored/shop_bootstrap.html',context_dict)
 
 
 def new_in(request, search = None, page = 1):
+	"""This view shows the new items in the website."""
 	context_dict = {}
 	items = []	
 	search = search
+
 	if search != None:
 		search = search.split(" ")
 		toAdd = []
+		
 		for word in search:
-
 			toAdd += Item.objects.filter(Q(description__contains = word ) | Q(title__contains = word)
 				| Q(category = word) | Q(section = word))
+			
 			for item in toAdd:
-				if (date.today()- item.datePosted).days<=7:
-					items+=[item]
-		searchS="_".join(search)
+				if (date.today()- item.datePosted).days <= 7:
+					items += [item]
+		
+		searchS = "_".join(search)
 		maxi = 0
+		
 		for item in items:
 			if item.price > maxi:
 				maxi = item.price
-		form = Search_bar()
-		context_dict['search_bar']=form
+		
+		serch_form = Search_bar()
+		context_dict['search_bar'] = search_form
 		context_dict['maxi'] = maxi + 5
 		context_dict['search'] = searchS
 		context_dict['page']  = page
@@ -346,27 +355,32 @@ def new_in(request, search = None, page = 1):
 		context_dict['pages'] = int(len(items)/6)
 		context_dict['min'] = 6 * (int(page) - 1)
 		context_dict['max'] = 6 * (int(page))
+		
 		return render(request, 'tailored/shop_bootstrap.html',context_dict)
 	else:
 		return home_page(request)
 
 
+def terms_and_conditions(request):
+	"""This view shows the terms and conditions of the website."""
+	return render(request, 'tailored/terms_and_conditions.html', {})
+
+
 def first_visit(request, response, item):
-	
+	"""This function checks if the given request is the first visit from that computer to the given item."""
+	if request.user == item.seller.user:
+		return False
+
 	last_visit_cookie = request.COOKIES.get('last_visit' + item, "first")
 	
-	if(last_visit_cookie == "first"):
+	if last_visit_cookie == "first":
 		response.set_cookie('last_visit' + item, str(datetime.now()))
 		return True
 	else:
-		last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-		if ((datetime.now() - last_visit_time).days > 0):
+		last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+		if (datetime.now() - last_visit_time).days > 0:
 			response.set_cookie('last_visit' + item, str(datetime.now()))
 			return True
 		else:
 			response.set_cookie('last_visit' + item , last_visit_cookie)
 			return False
-
-
-def terms_and_conditions(request):
-	return render(request, 'tailored/terms_and_conditions.html', {})
